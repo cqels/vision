556ca9a398ce2872c55b3813993ef8d6de660628
Manh Nguyen Duc <ducmanh93vietnam@gmail.com>
2021-08-17 15:26:13 +0200

    add query processing

---
+++ src/utils/data.py
@@ -0,0 +1,4 @@
+
+
+def prepare_data():
+    print("not implemented!")
--- src/utils/data_processing.py
+++
@@ -1,9 +0,0 @@
-
-
-def prepare_data():
-    print("not implemented!")
-
-
-def validate_data(path=None):
-    print(os.listdir("."))
-    return path
