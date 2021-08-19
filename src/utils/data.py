7dd536907a7b75f30ed3cfef939655af80e437bf
Manh Nguyen Duc <ducmanh93vietnam@gmail.com>
2021-08-19 16:36:03 +0200

    add query processing

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,6 +1,13 @@
 import os
+import urllib.request
 
 DATASETS = ['cityscapes', 'coco', 'detrac', 'kitti', 'oid', 'visual_genome']
+DATASETS_URLS = {'cityscapes': {'train': '', 'val': '', 'tearm_and_conditions': ''},
+                 'coco': {'train': 'http://images.cocodataset.org/zips/train2017.zip', 'val': 'http://images.cocodataset.org/zips/val2017.zip', 'tearm_and_conditions': 'By downloading this dataset, you agree to COCO\'s Terms of Use (https://cocodataset.org/#termsofuse).'},
+                 'detrac': {'train': '', 'val': '', 'tearm_and_conditions': ''},
+                 'kitti': {'train': '', 'val': '', 'tearm_and_conditions': ''},
+                 'oid': {'train': '', 'val': '', 'tearm_and_conditions': ''},
+                 'visual_genome': {'train': '', 'val': '', 'tearm_and_conditions': ''}}
 
 
 def list_datasets():
@@ -21,7 +28,19 @@ def prepare_data(images, DATA_ROOT_PATH="/mnt"):
         if os.path.isdir(path):
             dataset_list[tmp[-1]]['isExist'] = True
         if not os.path.exists(DATA_ROOT_PATH + image['image_path']):
-            dataset_list[tmp[-1]]['missing'].append(image)
+            isSuccess = False
+            if image['url']:
+                print("Image", image['file_name'],
+                      'is available online. Downloading..')
+                try:
+                    urllib.request.urlretrieve(image['url'], "00000001.jpg")
+                    isSuccess = True
+                    print(image['file_name'], "downloaded!")
+                except:
+                    isSuccess = False
+                    print(image['file_name'], "download failed!")
+            if not isSuccess:
+                dataset_list[tmp[-1]]['missing'].append(image)
     for dataset in dataset_list:
         if not dataset_list[dataset]['isExist']:
             print(dataset, "is not exist! Please download and put it at",
@@ -31,3 +50,13 @@ def prepare_data(images, DATA_ROOT_PATH="/mnt"):
                 print("The following images are not exists. Please download and put them at",
                       dataset_list[dataset]['path'], ":")
                 print(",".join(dataset_list[dataset]['missing']))
+
+    print("dataset folder structure should be prepare as bellow:")
+    print("DATA_ROOT_PATH/data/image_dataset")
+    print("  |-- dataset name")
+    print("    |-- images")
+    print("      |-- xxxxxx.jpg")
+    print("      |-- xxxxx2.jpg")
+    print("      |-- ...")
+    print("    |-- annotations")
+    print("      |-- annotation1.json")
