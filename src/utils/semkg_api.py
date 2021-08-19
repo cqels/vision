000d57bef7bad6fbf83075f20250aa890649028a
Manh Nguyen Duc <ducmanh93vietnam@gmail.com>
2021-08-19 15:53:53 +0200

    add query processing

--- src/utils/semkg_api.py
+++ src/utils/semkg_api.py
@@ -1,7 +1,8 @@
 import requests
+import json
 
 
 def query(query_string, token=""):
     response = requests.post('https://vision-api.semkg.org/api/querydtd',
                              json={"query": query_string, token: token})
-    return response.json()
+    return json.loads(response.json())
