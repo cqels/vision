8c519060e9540be13545f3aa86dc02a6b7eae2cf
Manh Nguyen Duc <ducmanh93vietnam@gmail.com>
2021-10-13 13:53:16 +0200

    show images

--- src/vision_utils/semkg_api.py
+++ src/vision_utils/semkg_api.py
@@ -2,7 +2,7 @@
 import requests
 import json
 
-SEMKG_IMAGES_HOST = "http://odsdemo01.service.tu-berlin.de"
+SEMKG_IMAGES_HOST = "https://files.semkg.org"
 def query(query_string, token=""):
     response = requests.post('https://vision-api.semkg.org/api/querydtd',
                              json={"query": query_string, token: token})
