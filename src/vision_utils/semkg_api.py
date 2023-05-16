4071935ec465cabac6907ec2bfe24ddf43930591
jichengYUAN <87916250+jichengyuan@users.noreply.github.com>
2023-05-16 10:21:07 +0200

    Update semkg_api.py

--- src/vision_utils/semkg_api.py
+++ src/vision_utils/semkg_api.py
@@ -4,6 +4,8 @@ import json
 
 SEMKG_IMAGES_HOST = "https://files.semkg.org"
 def query(query_string, token=""):
-    response = requests.post('https://vision-api.semkg.org/api/querydtd',
+#     response = requests.post('https://vision-api.semkg.org/api/querydtd',
+#                              json={"query": query_string, token: token})
+    response = requests.post('https://vision.semkg.org/sparql',
                              json={"query": query_string, token: token})
     return json.loads(response.json())
