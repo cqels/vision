bfd5c833b4f59955252425c854db07f1b9b2cbad
jichengYUAN <87916250+jichengyuan@users.noreply.github.com>
2023-05-17 12:48:46 +0200

    Update semkg_api.py
    temp_api for visionkg2.0

--- src/vision_utils/semkg_api.py
+++ src/vision_utils/semkg_api.py
@@ -6,6 +6,9 @@ SEMKG_IMAGES_HOST = "https://files.semkg.org"
 def query(query_string, token=""):
 #     response = requests.post('https://vision-api.semkg.org/api/querydtd',
 #                              json={"query": query_string, token: token})
-    response = requests.post('https://vision.semkg.org/sparql',
-                             json={"query": query_string, token: token})
+#     response = requests.post('https://vision.semkg.org/sparql',
+#                              json={"query": query_string, token: token})
+# temp api
+      response = requests.post('https://vision-api.semkg.org/api/sparql_temp',
+                               json={"query": query_string, token: token})
     return json.loads(response.json())
